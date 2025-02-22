437c371fd027d4cdb27e099f29452da630d278fa
AmberLynnR33 <103320792+AmberLynnR33@users.noreply.github.com>
2025-02-22 14:21:12 -0500

    Making .wav file from .mid
    Didn't work in code. Did on Garage Band

--- UI/app.py
+++ UI/app.py
@@ -132,4 +132,4 @@ elif visual_selection == "Misleading Piece":
 
     st.write("TODO: Figure caption")
 elif visual_selection == "Counter Data":
-    st.audio("audio/richardson_counterdatapiece.mid")
+    st.audio("audio/richardson_counterdatapiece.wav")
