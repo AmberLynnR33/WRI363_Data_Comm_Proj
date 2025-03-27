4d54edb93ca6875b7482329e732947f721ab3f11
AmberLynnR33 <103320792+AmberLynnR33@users.noreply.github.com>
2025-03-27 14:27:48 -0400

    Trying to get audio to work

--- UI/streamlit_app.py
+++ UI/streamlit_app.py
@@ -298,7 +298,7 @@ elif visual_selection == "Counter Data Piece":
     st.header("Counter Data: Representing Date vs Card Pulled in an Audio Format")
 
     # audio
-    st.audio("audio/counterdatapiece.wav")
+    st.audio("counterdatapiece.wav")
 
     # Figure Caption
     st.markdown('''A representation of card draws by day created in MIDI audio format. 
